e446cac21d5421a84eb9ce6efa578de5d19714cd
Tiagoroccatagliata <tiagoroccatagliata@gmail.com>
2024-05-30 13:20:43 -0300

    Operadores

---
+++ Variables.php
@@ -0,0 +1,16 @@
+<?php
+    // Variables: string, integer, float, boolean.
+    $nombre = "Tiago";
+    $comida = "Pizza";
+    $email = "fake@gmail.com";
+
+    $edad = 24;
+    $usuarios = 2;
+
+    echo"Hola {$nombre}<br>";
+    echo"Te gusta la {$comida}<br>";
+    echo"tu email es {$email}<br>";
+
+    echo"Tu edad es {$edad}<br>";
+    echo"Hay {$usuarios} usuarios";
+?>
